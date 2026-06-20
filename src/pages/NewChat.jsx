f98661c3c67f414d3d697845baae797d1ae3908c
johnnathanieI <117893352+johnnathanieI@users.noreply.github.com>
2026-06-20 04:09:47 -0600

    Messing around, need to switch SBButton to use icon props for unique icons instead of img (svg)

--- src/pages/NewChat.jsx
+++ src/pages/NewChat.jsx
@@ -16,7 +16,7 @@ export default function NewChat() {
               stroke-width="2"
               stroke-linecap="round"
               stroke-linejoin="round"
-              class="lucide lucide-plus-icon lucide-plus"
+              className="lucide lucide-plus-icon lucide-plus"
             >
               <path d="M5 12h14" />
               <path d="M12 5v14" />
@@ -40,7 +40,7 @@ export default function NewChat() {
               stroke-width="2"
               stroke-linecap="round"
               stroke-linejoin="round"
-              class="lucide lucide-arrow-up-icon lucide-arrow-up"
+              className="lucide lucide-arrow-up-icon lucide-arrow-up"
             >
               <path d="m5 12 7-7 7 7" />
               <path d="M12 19V5" />
